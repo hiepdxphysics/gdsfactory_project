42a344585e0ff2c8b43ecf9cb82688e119c5921e
hiepxdinh <s3923133@student.rmit.edu.au>
2023-11-21 12:35:05 +1100

    add changes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import gdsfactory as gf
 from gdsfactory.dft.siepic import add_fiber_array_siepic
+from gdsfactory.components import spiral
 from ubcpdk.components.grating_couplers import gc_te1550
+from ubcpdk.components.cells import straight
 
 add_fiber_array = gf.partial(
     add_fiber_array_siepic, gc_port_name="opt1", grating_coupler = "gc_te1550"
 )
 
-
+ring = gf.partial(mzi)
 
 if __name__ == "__main__":
     print("Hello")
-    c = add_fiber_array()
+
+    # c = spiral(length=10e3, port_spacing=500, radius=20, direction="EAST")
+    c = add_fiber_array(spiral)
     c.show()
+    c.write_gds_with_metadata("demo.gds")
